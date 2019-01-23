2ad70fec371327b5f88f92fa1674165575bac7a9
hz <goforzzz@gmail.com>
2019-01-23 21:18:04 +0800

    ac 154

---
+++ code/woj154.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	int count = 1;
+	while (cin >> s && s != "#")
+	{
+		for (int i = 0; i < s.length(); i++)
+			if ((i + 1) % 2 == 0)
+				s[i] = toupper(s[i]);
+		cout << "Case " << count << ": " << s << endl;
+		count++;
+	}
+}
