eee214adda4fbe353d861d927b20350cdbba7e25
hz <goforzzz@gmail.com>
2019-01-23 22:06:37 +0800

    ac 157

---
+++ code/woj157.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, count = 1;
+	while (cin >> n && n != -1)
+	{
+		int tmp, collision = 0, sum = 0;
+		for (int i = 0; i < n; i++)
+		{
+			cin >> tmp;
+			if (tmp == 1)
+				collision++;
+			else
+				sum += collision;
+		}
+		cout << "Case " << count << ": " << sum << endl;
+		count++;
+	}
+}
