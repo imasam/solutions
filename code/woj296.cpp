312dcca99760d3a14ae341c00528b2dcbbe31cfd
hz <goforzzz@gmail.com>
2019-01-24 21:40:48 +0800

    ac 296

---
+++ code/woj296.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int n;
+	while (cin >> n && n != 0)
+		cout << (n % 2 ? to_string(n - 1) : "No Solution!") << endl;
+	return 0;
+}
