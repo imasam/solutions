acd37747de1941c75d1240e633a7029ac0717fd3
hz <goforzzz@gmail.com>
2019-01-24 21:46:03 +0800

    ac 300

---
+++ code/woj300.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int c;
+	cin >> c;
+	while (c--)
+	{
+		int n;
+		cin >> n;
+		cout << (n % 7 == 0 || n % 7 == 1 ? "Dzs" : "Sproblvem") << endl;
+	}
+	return 0;
+}
