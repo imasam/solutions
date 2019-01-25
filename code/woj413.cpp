d96db28e5ef004f732034499674900c377154c1e
hz <goforzzz@gmail.com>
2019-01-25 22:49:07 +0800

    ac 413(stupid)

---
+++ code/woj413.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int a, b, h;
+		cin >> a >> b >> h;
+		cout << setiosflags(ios::fixed) << setprecision(2) << 1.0 * b / a * h << endl;
+	}
+}
