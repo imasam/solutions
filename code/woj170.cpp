6c51a1aff9d9bc2aa091dd3b6f4398ddaf271d76
hz <goforzzz@gmail.com>
2019-01-26 16:35:17 +0800

    ac stupid 170

---
+++ code/woj170.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <set>
+using namespace std;
+
+int main()
+{
+	int n;
+	set<int> data;
+	while (cin >> n && n != 0)
+		data.insert(n);
+	for (auto it = data.begin(); it != data.end(); it++)
+		cout << *it << " ";
+	cout << endl;
+	return 0;
+}
