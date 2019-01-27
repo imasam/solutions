c7f29763da3c7cadda95deeff4e8d0477f0e6fa3
hz <goforzzz@gmail.com>
2019-01-27 21:14:37 +0800

    ac 213

---
+++ code/woj213.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+struct point
+{
+	int x;
+	int y;
+};
+
+int main()
+{
+	int n;
+	while (cin >> n)
+	{
+		vector<point> arr(n);
+		double area = 0;
+		for (int i = 0; i < n; i++)
+			cin >> arr[i].x >> arr[i].y;
+		for (int i = 0; i < n - 1; i++)
+			area += 0.5 * (arr[i].x*arr[i + 1].y - arr[i + 1].x*arr[i].y);
+		area += 0.5 * (arr[n - 1].x*arr[0].y - arr[0].x*arr[n - 1].y);
+		cout << fabs(area) << endl;
+	}
+	return 0;
+}
