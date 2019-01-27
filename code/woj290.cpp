fe46f870601e801bb716f37cddf04412e2ab62c6
hz <goforzzz@gmail.com>
2019-01-27 16:37:06 +0800

    ac 290 (the most stupid problem labelled hard in woj)

---
+++ code/woj290.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	int n;
+	while (cin >> n && n != 0)
+	{
+		vector<int> arr(n);
+		for (int i = 0; i < n; i++)
+			cin >> arr[i];
+		int result = 0;
+		for (int i = 0; i < n; i++) 
+			for (int j = 1; j < n - i; j++) 
+				if (arr[j] < arr[j - 1]) 
+				{
+					int tmp = arr[j - 1];
+					arr[j - 1] = arr[j];
+					arr[j] = tmp;
+					result++;
+				}
+		cout << result << endl;
+	}
+	return 0;
+}
