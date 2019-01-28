fe974e9b10f2f7931b21977a92cf1aad0f6bedf2
hz <goforzzz@gmail.com>
2019-01-28 19:33:52 +0800

    ac 183 : modify this one line program to pass cheat detection:)

---
+++ code/woj184.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	int n;
+	while (cin >> n)
+	{
+		if (n <= 0 || n > 10000)
+			break;
+		int result = sqrt(n);
+		cout << result << endl;
+	}
+	return 0;
+}
